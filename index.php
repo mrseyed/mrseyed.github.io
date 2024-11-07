bfc6677bc94effc4cefa55e6a22cbdf5126fd95b
Mohandes Mousavi <moosavi.sayyed@gmail.com>
2024-11-07 20:27:11 +0330

    MrSeyed

---
+++ index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="fa">
+<head>
+
+<meta id="mtKeywords" name="KEYWORDS" content="">
+
+<meta property="og:type" content="article" />
+<meta property="og:url" content="https;//mrseyed.github.io" />
+<meta property="og:locale" content="fa_IR" />
+<meta property="og:site_name" content="https://mrseyed.github.io" />
+<meta property="og:title" content="آقاسیــد" />
+<meta property="og:description" content="
+ارائه کلیه خدمات تخصصی سیستم عامل اندروید و اینترنتی و فروسگاه اینترنتی محصولات مرتبط
+" />
+<meta property="og:image" content="https://mrseyed.github.io/media/mrseyed.png">
+<title>آقــا  سـیـــد</title>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset:utf-8" />
+<meta http-equiv="Content-language" content="fa" />
+<meta name="viewport" content="width=device-width">
+ <link rel="stylesheet" type="text/css" href="css/00.css">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+ <link rel="stylesheet" type="text/css" href="css/tamine1.css">
+ <link rel="stylesheet" type="text/css" href="msdata/fonts/all.css">
+ <link rel="stylesheet" type="text/css" href="msdata/fonts/fa.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/fonts/icofont.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/fonts/fonts.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/4/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/5/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/4/bootstrap-reboot.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/5/bootstrap-reboot.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/4/bootstrap-grid.min.css">
+ <link rel="stylesheet" type="text/css" href="msdata/css/5/bootstrap-grid.min.css">
+<script src="js/jquery.min.js" ></script>
+<script src="js/4/bootstrap.min.js"></script>
+<script src="js/5/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/tarikh.js"></script>
+</head>
+<body>
+<center>
+<div class="body" >
+
+ <center>
+ <header>
+ 
+  <div class="head-t" align="center" style="text-align:center" >
+    <center>
+    <center><span id="sahelb" class="head-t0" style="" align="right" >آقــا</span>
+    <span id="sahelb" class="head-t1" style="" >سـیــــد</span><br>
+    <span id="sahelb" class="head-t2"  style="margin-right:-50px;text-align:right" >مـهـنـــدس مـوســوی</span><br>
+    <span id="sahelb" class="head-t3" style="" >انـدرویـد</span>
+        <span id="sahelb" class="head-t5" style="color:red" >و</span>
+    <span id="sahelb" class="head-t4" style="" >اینتــرنت</span><br>
+  <span ></span>
+  </div>
+  <div class="divhead" >
+   <img src="media/fram06.jpg" >
+  </div>
+
+ </header>
+ 
+ 
+ <br>
+ <div class="div-time" align="center" >
+  <table id="led" style="" >
+   <tr>
+    <th><span id="ms" ></span></th>
+    <th>.</th>
+    <th><span id="md" ></span></th>
+    <th>:</th>
+    <th><span id="mh" ></span></th>
+   </tr>
+  </table>
+ </div>
+<script type="text/javascript" src="js/saat.js"></script>
+ 
+  <div class="div-day" align="center" >
+   <span id="dhafte" style="font-family:vazirbd" ></span>
+  </div>
+ <div class="div-date" align="center" >
+  <table id="vazir" >
+   <tr> 
+    <th><span id="neww" ></span></th>
+    <th>/</th>
+    <th>1403</th>
+   </tr>
+  </table>
+ </div>
+  <script type="text/javascript" src="js/newt.js"></script>
+<script type="text/javascript">
+var date = new Date();
+var hafte = date.getDay();
+var shanbe = [" یکشنبه "," دوشنبه ", " سه‌شنبه ", " چهارشنبه ", " پنجشنبه ", " جمعه ", " شنبه "];
+document.getElementById("dhafte").innerHTML = shanbe[hafte];
+</script>
+<script type="text/javascript">
+document.getElementById("neww").innerHTML = bm[today];
+</script>
+	
+
+  <div class="div-fram" >
+   <button class="btn dark" id="btn-menu" style="margin-top:30px" >صفحه اصلی</button>
+   <button class="btn gold" id="btn-menu" style="margin-top:30px">اندروید</button>
+   <button class="btn red" id="btn-menu" style="margin-top:30px">اینترنت</button>
+   <br>
+   <button class="btn orange" id="btn-menu">برنامه</button>
+   <button class="btn normal" id="btn-menu">فروشگـاه</button>
+   <button class="btn gray" id="btn-menu">ارتباط با ما</button>
+
+     
+  </div>
+<br><br>
+
+<center>
+  <span class="title-post1" >##</span>
+  <span class="title-post2" >##</span>
+  <img src="media/blu/baner.webp" style="width:380px;height:110px" >
+  
+  
+  
+  
+  <div class="form-post"  > 
+   <br><br>
+     <div id="vazir" align="center" >
+      <table align="right" class="tb-ax" >
+       <tr class="bg-warning" >
+        <th id="sahelb" >#</th>
+       </tr>
+       <tr>
+        <td>#</td>
+       </tr>
+       <tr class="bg-secondary" >
+        <th id="sahelb" >##</th>
+       </tr>
+       <tr>
+        <td>##</td>
+       </tr>
+       <tr class="bg-danger" >
+        <th id="sahelb" >##</th>
+       </tr>
+       <tr>
+        <td style="text-align:center; font-size:13px" ><dt>
+*
+        </dt></td>
+       
+       </tr>
+       
+      </table>
+      <div align="left" style="width:165px;margin-right:200px" >
+       <img src="media/logo.1.png" style="width:160px" >
+      </div>
+      
+     </div>
+   <br>
+
+
+
+  </div>
+
+    
+</div>
+  <br>
+  <br>
+</center>
+</body>
+
+<script src="msdata/js/jquery.slim.min.js" ></script>
+<script src="msdata/js/4/bootstrap.bundle.min.js"></script>
+<script src="msdata/js/5/bootstrap.bundle.min.js"></script>
+</html>
