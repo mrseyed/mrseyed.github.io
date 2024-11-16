d56121720bad57f41132f7c0a570d89ccdcfb5d7
Mohandes Mousavi <moosavi.sayyed@gmail.com>
2024-11-17 01:25:22 +0330

    MrSeyed

--- index.php
+++ index.php
@@ -154,7 +154,20 @@ document.getElementById("neww").innerHTML = bm[today];
      </div>
    <br>
 
-
+<?php
+   if( $_REQUEST["name"] || $_REQUEST["age"] ) {
+      echo "Welcome ". $_REQUEST['name']. "<br />";
+      echo "You are ". $_REQUEST['age']. " years old.";
+      exit();
+   }
+?>
+      
+      <form action = "<?php $_PHP_SELF ?>" method = "POST">
+         Name: <input type = "text" name = "name" />
+         Age: <input type = "text" name = "age" />
+         <input type = "submit" />
+      </form>
+      
 
   </div>
 
