dc7cb23544fd32f7425c2776c13143d13662231b
MrSeyed <moosavi.sayyed@gmail.com>
2025-08-07 17:29:05 +0330

    MrSeyed

---
+++ zp/zpverify.php
@@ -0,0 +1,301 @@
+
+<html dir="rtl" lang="fa">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TWQXJ7VZ');</script>
+<!-- End Google Tag Manager -->
+
+<meta id="mtKeywords" name="KEYWORDS" content="">
+
+<meta property="og:type" content="article" />
+<meta property="og:url" content="https;//mrseyed.github.io" />
+<meta property="og:locale" content="fa_IR" />
+<meta property="og:site_name" content="https://mrseyed.github.io" />
+<meta property="og:title" content="آقاسیــد" />
+<meta property="og:description" content="
+استخدام کارخانه اغذیه و کیک آقاسید کرمان(پرداخت هزینه)
+" />
+<meta property="og:image" content="https://mrseyed.github.io/media/factorylogo0.png">
+<title>پرداخت هزینه</title>
+
+
+<meta http-equiv="Content-Type" content="text/html; charset:utf-8" />
+<meta http-equiv="Content-language" content="fa" />
+<meta name="viewport" content="width=device-width">
+
+ <link rel="stylesheet" type="text/css" href="./../css/style.css">
+
+
+ <link rel="stylesheet" type="text/css" href="https://mrseyed.github.io/msdata/fonts/fa.min.css">
+ <link rel="stylesheet" type="text/css" href="https://mrseyed.github.io/msdata/fonts/icofont.min.css">
+ <link rel="stylesheet" type="text/css" href="https://mrseyed.github.io/msdata/fonts/fonts.css">
+
+
+
+ <script type="text/javascript" src="./../msdata/js/persian-date.js"></script>
+</head>
+<style type="text/css">
+
+.divmain {
+width: 380px;
+border: 3px ridge gray;
+border-radius:8px;
+background-color: #e6ffe6;
+font-family: irsblack;
+font-size:17px;
+}
+.success {
+font-size:40px;
+color:#990000;
+}
+.nosuccess {
+font-size:40px;
+color:white;
+}
+.i1 {
+color:#1f7a1f;
+}
+.i2 {
+color: yellow;
+}
+.divbody {
+border:3px ridge gray;
+border-top:0px;
+width:365px;
+font-family:irsbold;
+font-size:20px;
+
+margin-bottom:10px;
+}
+.p-chand {
+font-family:sahelblack;
+font-size:30px;
+margin-top:-5px
+}
+.pchand2 {
+font-family:sahelblack;
+font-size:30px;
+margin-top:-5px;
+color:white;
+}
+.p-pool {
+font-family:irsbold;
+font-size:40px;
+font-weight:1000;
+color:gold;
+
+}
+.sayer {
+width:90%;
+background-color:#e6f2ff;
+font-size:16px;
+font-family:vazir;
+color:black;
+font-weight:1000;
+border:0.5px solid black;
+padding:5px
+}
+.infosuccess {
+width:370px;
+border:3px ridge gray;
+border-radius: 15pt 0 15pt 0;
+font-family:vazirbk;
+font-size:20px;
+color: darkblue;
+background-color:#f0f0f5;
+}
+h2 {
+font-family:titr2;
+margin-top:20px;
+}
+.hr {
+border-top:4px double red;
+width:95%;
+
+}
+input {
+font-family:irsbold;
+font-size:40px;
+width:90%;
+text-align:center;
+color:red;
+border: 5px ridge brown;
+border-radius:10pt;
+background-color:lightyellow;
+}
+.bg-dark {
+background-color:black;
+}
+.bg-danger {
+background-color:brown;
+}
+body {
+background-image:url("https://mrseyed.github.io/ax.jpg");
+}
+</style>
+<body>
+<?php
+$Status = $_GET['Status'];
+?>
+<input type="hidden" value="<?php echo $Status; ?>" id="vok" >
+
+
+
+	<center>
+	<hr>
+	<div id="fsuccess" hidden="hidden" >
+	 <div class="divmain">
+	  <span class="success" >
+	   <i class="fa fa-square-check i1"></i>
+تراکنش موفق
+	   <i class="fa fa-thumbs-up i1"></i>
+	  </span>
+	 </div>
+	 <div class="divbody" >
+	  <div class="receipt-title bg-dark" >
+	   <span class="p-chand" >مبلغ</span>
+	   <span class="p-pool">259,000</span>
+	   <span class="p-chand" >ریال</span>
+	  </div>
+	  
+	  <div class="sayer" >
+	   <div class="receipt-row">
+	    <span>تاریخ</span>
+	    <span></span>
+	    <span id="tarikh" ></span>
+	   </div>
+	   <div class="receipt-row">
+	    <span>ساعت</span>
+	    <span></span>
+	    <span id="saat" ></span>
+	   </div>
+	   
+	   <div class="receipt-row">
+	    <span>شماره سفارش</span>
+	    <span></span>
+	    <span id="factoor1" ></span>
+	   </div>
+	   <div class="receipt-row">
+	    <span>شماره پیگیری</span>
+	    <span></span>
+	    <span id="pcode1" ></span>
+	   </div>
+	  </div>
+	  <div class="receipt-end bg-dark" style="font-family:titr2;font-size:25px;width:380px;color:yellow" >
+پرداخت شما با موفقیت انجام شد.
+	   </div>
+	 </div>
+	 <div class="infosuccess" >
+	  <h2>«ثبت‌نام شما باموفقیت انجام شد»</h2>
+	  <div class="hr" ></div>
+	  <q style="color:red" >پس از بررسی درخواست شما توسط همکاران ما، یک پیامک تاییدیه برایتان ارسال خواهد شد و تاریخ و ساعت مصاحبه را خدمت حضورتان اعلام می‌کند.</q>
+	  <div class="hr" ></div>
+	  <span style="font-family:shabnambold;font-size:18px" ><i style="font-size:25x;color:orange" class="icofont-warning" ></i>
+برای پیگیری بهتر پرداخت شما، بهتر است کد سفارش زیر را ازطریق واتساپ به شماره زیر ارسال کنید.
+	  </span>
+	  <fieldset>
+	   <legend style="text-align:center" >کدسفارش</legend>
+	   <input type="text" readonly="readonly" id="factoor3"> 
+	  </fieldset>
+	  <a href="https://wa.me/989377846588" >
+	  <img src="https://mrseyed.github.io/media/whatsapp.webp" style="width:360px">  
+	  </a>
+	  
+	 </div>
+	 
+	</div>
+	
+	<hr>
+	
+	
+	
+	
+	<div id="fnosuccess" hidden="hidden" >
+	 <div class="divmain bg-danger" >
+	  <span class="nosuccess" >
+	   <i class="fa fa-circle-xmark i2"></i>
+	 تراکنش ناموفق
+	   <i class="fa fa-face-frown i2"></i>
+	  </span>
+	 </div>
+	 <div class="divbody bg-danger" >
+	  <div class="receipt-title2 bg-dark" >
+	   <span class="p-chand2" >مبلغ</span>
+	   <span class="p-pool">229,000</span>
+	   <span class="p-chand2" >ریال</span>
+	  </div>
+	  <center>
+	  <div class="sayer" >
+	   <div class="receipt-row">
+	    <span>تاریخ</span>
+	    <span></span>
+	    <span id="tarikh2" ></span>
+	   </div>
+	   <div class="receipt-row">
+	    <span>ساعت</span>
+	    <span></span>
+	    <span id="saat2" ></span>
+	   </div>
+	   
+	   <div class="receipt-row">
+	    <span>کدرهگیری</span>
+	    <span></span>
+	    <span id="pcode2" ></span>
+	   </div>
+	   <div class="receipt-row">
+	    <span>شماره سفارش</span>
+	    <span></span>
+	    <span id="factoor2" ></span>
+	   </div>
+	   </center>
+	  </div>
+	  <div class="receipt-end2 bg-danger" style="font-family:saminbold;font-size:25px;width:380px" >
+	 پرداخت شما ناموفق بود.درصورت کسر وجه حداکثر تا 72ساعت آینده به حسابتان برگشت خواهد خورد.
+	   </div>
+	 </div>
+	 <div class="infosuccess" >
+	 <q>متاسفانه ثبت نام شما نیز لغو گردید.لطفا فرم ثبت نام را مجددا تکمیل نمایید.</q>
+	 
+	 </div>
+	 
+	 
+	</div>
+	</center>
+<script type="text/javascript">
+var vok = document.getElementById("vok").value;
+	if(vok === "OK"){
+	document.getElementById("fsuccess").hidden=false;
+	document.getElementById("fnosuccess").hidden=true;
+	}else{
+	document.getElementById("fsuccess").hidden=true;
+	document.getElementById("fnosuccess").hidden=false;
+	
+	}
+
+
+</script>
+</body>
+<script type="text/javascript">
+	 var mydate = new persianDate();
+	 var tarikh = mydate.format("YYYY/MM/DD");
+	 var saat = mydate.format("HH:mm.ss");
+	 var pcode = mydate.format("YYYYMMDDMMDDDDMYY");
+	 var factoor = mydate.format("dYYMDDDDmHHmm");
+	 
+	 document.getElementById("tarikh").innerHTML=tarikh;
+	 document.getElementById("saat").innerHTML=saat;
+	document.getElementById("tarikh2").innerHTML=tarikh;
+	document.getElementById("saat2").innerHTML=saat;
+	document.getElementById("pcode1").innerHTML=pcode;
+	document.getElementById("pcode2").innerHTML=pcode;
+	document.getElementById("factoor1").innerHTML=factoor;
+	document.getElementById("factoor2").innerHTML=factoor;
+	document.getElementById("factoor3").value=factoor;
+
+</script>
+</html>
